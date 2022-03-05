82d782e5a9e3f95638db6711375cd2a253344dd5
Piotr Ponikowski <piopon.github@gmail.com>
2022-03-05 01:21:17 +0100

    added simple model classes: Word and Round

--- src/main/scala/pl/smtc/service/SmartWordsApp.scala
+++ src/main/scala/pl/smtc/service/SmartWordsApp.scala
@@ -2,4 +2,7 @@ package pl.smtc.service
 
 object SmartWordsApp extends App {
 
+  case class Word(name: String, category: String, definition: String)
+  case class Round(word: Word, options: List[String], answer: String)
+
 }

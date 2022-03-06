5292841472b5fad15e117f0df64fc42f4aa9c96a
Piotr Ponikowski <piopon.github@gmail.com>
2022-03-06 12:09:09 +0100

    thinking of service API and logic flow

--- src/main/scala/pl/smtc/service/SmartWordsApp.scala
+++ src/main/scala/pl/smtc/service/SmartWordsApp.scala
@@ -10,4 +10,19 @@ object SmartWordsApp extends App {
   case class Round(word: Word, options: List[String], answer: String)
   case class Quiz(rounds: Map[Round, Boolean], score: Int)
 
+  /*
+   * API ENDPOINTS + APPLICATION FLOW
+   * - POST: start new quiz (how many questions/answers immediately) -> OK / error (to little words)
+   * - GET: questionNo (1 ... 10) -> OK + JSON with word and 4 options
+   * - POST: questionNo/answerNo -> OK + JSON correct/incorrect answer
+   * - GET: result -> OK + JSON with score %
+   * - GET: summary -> OK + list of all questions with answers and correct definitions
+   *
+   * ADMIN COMMANDS
+   * - GET: all words
+   * - GET: all words of selected category
+   * - POST: add a new word
+   * - DELETE: remove a word
+   * - PUT: modify a word
+   */
 }

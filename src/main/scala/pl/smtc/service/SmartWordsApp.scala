2c49ef3490eecfa9a09c72fbc007b2abe4f02a9e
Piotr Ponikowski <piopon.github@gmail.com>
2022-03-26 00:06:26 +0100

    updated category enum with fromString (used in url parsing)

--- src/main/scala/pl/smtc/service/SmartWordsApp.scala
+++ src/main/scala/pl/smtc/service/SmartWordsApp.scala
@@ -26,7 +26,9 @@ object SmartWordsApp extends IOApp {
    */
   object Category extends Enumeration {
     type Category = Value
-    val verb, adverb, noun, adjective = Value
+    val verb, adverb, noun, adjective, unknown = Value
+    def fromString(string: String): Value =
+      values.find(_.toString.toLowerCase() == string.toLowerCase()).getOrElse(unknown)
   }
 
   /**

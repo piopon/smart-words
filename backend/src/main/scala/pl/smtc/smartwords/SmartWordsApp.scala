d4ef73b3465c7e7fac3c76b1d6fb96a80d96bca0
Piotr Ponikowski <piopon.github@gmail.com>
2022-04-23 00:39:15 +0200

    moved Round case class to a separate file

--- backend/src/main/scala/pl/smtc/smartwords/SmartWordsApp.scala
+++ backend/src/main/scala/pl/smtc/smartwords/SmartWordsApp.scala
@@ -44,13 +44,6 @@ object SmartWordsApp extends IOApp {
     testWordDB.toList.filter(word => word.category.equals(category))
   }
 
-  /**
-   * Model class representing a single round of a quiz
-   * @param word selected word which a user has to figure out
-   * @param options the list of possible answers (will be matched from word category)
-   * @param correct true if got correct answer, false otherwise. If no answer yet then None
-   */
-  case class Round(word: Word, options: List[String], var correct: Option[Boolean])
   implicit val RoundEncoder: Encoder[Round] = Encoder.instance {
     (round: Round) => json"""{"word": ${round.word.name}, "options": ${round.options}}"""
   }

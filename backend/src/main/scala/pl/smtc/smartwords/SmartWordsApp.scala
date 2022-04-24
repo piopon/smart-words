ec973ae68e36eccbd036603240f4ae72bfad9ddf
Piotr Ponikowski <piopon.github@gmail.com>
2022-04-24 14:49:59 +0200

    removed unused imports with main smart words app file

--- backend/src/main/scala/pl/smtc/smartwords/SmartWordsApp.scala
+++ backend/src/main/scala/pl/smtc/smartwords/SmartWordsApp.scala
@@ -7,13 +7,11 @@ import com.comcast.ip4s._
 import org.http4s.circe._
 import io.circe._
 import io.circe.syntax._
-import io.circe.generic.auto._
 import io.circe.literal._
 import io.circe.parser._
 import org.http4s._
 import org.http4s.dsl._
 import org.http4s.dsl.io._
-import org.http4s.headers._
 import org.http4s.implicits._
 import org.http4s.server._
 import org.http4s.ember.server._

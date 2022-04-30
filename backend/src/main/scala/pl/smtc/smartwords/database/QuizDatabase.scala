a4d5bb955004b34941f00ac721e6176b385b625b
Piotr Ponikowski <piopon.github@gmail.com>
2022-04-30 16:33:13 +0200

    updated quiz DB with add and remove methods

--- backend/src/main/scala/pl/smtc/smartwords/database/QuizDatabase.scala
+++ backend/src/main/scala/pl/smtc/smartwords/database/QuizDatabase.scala
@@ -10,4 +10,12 @@ class QuizDatabase {
   private val activeQuizzes: mutable.Map[UUID, Quiz] = mutable.Map()
 
   def getQuiz(uuid: UUID): Option[Quiz] = activeQuizzes.get(uuid)
+
+  def addQuiz(quiz: Quiz): UUID = {
+    val newUuid: UUID = UUID.randomUUID()
+    activeQuizzes.put(newUuid, quiz)
+    newUuid
+  }
+
+  def removeQuiz(uuid: UUID): Option[Quiz] = activeQuizzes.remove(uuid)
 }

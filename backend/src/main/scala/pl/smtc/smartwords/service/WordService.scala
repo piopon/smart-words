ebb57d6e4ebe24613308136d542df8dc999e8050
Piotr Ponikowski <piopon.github@gmail.com>
2022-05-05 00:49:28 +0200

    added new addWord and updateWord methods to words service class

--- backend/src/main/scala/pl/smtc/smartwords/service/WordService.scala
+++ backend/src/main/scala/pl/smtc/smartwords/service/WordService.scala
@@ -16,6 +16,23 @@ class WordService(wordDB: WordDatabase) {
     (word: Word) => json"""{"name": ${word.name}, "category": ${word.category.toString}, "description": ${word.definition}}"""
   }
 
+  def addWord(word: Word): IO[Response[IO]] = {
+    if (wordDB.addWord(word)) {
+      Ok(s"Added new word \"${word.name}\".")
+    } else {
+      Ok(s"Word \"${word.name}\" already defined.")
+    }
+  }
+
+  def updateWord(name: String, word: Word): IO[Response[IO]] = {
+    val nameIndex = wordDB.getWords.indexWhere((word: Word) => word.name.equals(name))
+    if (wordDB.updateWord(nameIndex, word)) {
+      Ok(s"Updated word \"$name\".")
+    } else {
+      NotFound(s"Word \"$name\" not found in DB.")
+    }
+  }
+
   def deleteWord(name: String): IO[Response[IO]] = {
     wordDB.getWordByName(name) match {
       case None => NotFound(s"Word \"$name\" not found in DB.")

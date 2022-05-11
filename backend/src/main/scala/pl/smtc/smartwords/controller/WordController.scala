2f6d27ba07d9aeea6593a4e98eaee1a0e509e8e3
Piotr Ponikowski <piopon.github@gmail.com>
2022-05-11 17:51:16 +0200

    created new word controller class with getRoutes logic

---
+++ backend/src/main/scala/pl/smtc/smartwords/controller/WordController.scala
@@ -0,0 +1,66 @@
+package pl.smtc.smartwords.controller
+
+import cats.effect._
+import com.comcast.ip4s._
+import org.http4s.circe._
+import io.circe._
+import io.circe.literal._
+import org.http4s._
+import org.http4s.dsl._
+import org.http4s.dsl.io._
+import org.http4s.implicits._
+import org.http4s.server._
+import org.http4s.ember.server._
+import pl.smtc.smartwords.database._
+import pl.smtc.smartwords.model._
+import pl.smtc.smartwords.service._
+
+class WordController(wordDB: WordDatabase) {
+
+  implicit val WordDecoder: Decoder[Word] = Decoder.instance {
+    (input: HCursor) => for {
+      name <- input.downField("name").as[String]
+      category <- input.downField("category").as[String]
+      definition <- input.downField("description").as[String]
+    } yield {
+      Word(name, Category.fromString(category), definition)
+    }
+  }
+
+  implicit val categoryParamDecoder: QueryParamDecoder[Category.Value] =
+    QueryParamDecoder[String].map(categoryStr => Category.fromString(categoryStr))
+  object OptionalCategoryParamMatcher extends OptionalQueryParamDecoderMatcher[Category.Value]("cat")
+
+  /**
+   * Routes (request -> response) for admin endpoints/resources
+   * <ul>
+   *  <li>Receive all words: <u>GET</u> /admin/words -> RET: OK 200 + ALL WORDS JSON / ERR 500</li>
+   *  <li>Receive category-specific words: <u>GET</u> /admin/words?cat=adj -> RET: OK 200 + Word JSON / ERR 500</li>
+   *  <li>Add a new word: <u>POST</u> /admin/words + Word JSON -> RET: OK 200 / ERR 500</li>
+   *  <li>Delete word: <u>DELETE</u> /admin/words/{name} -> RET: OK 200 / ERR 404</li>
+   *  <li>Update word: <u>PUT</u> /admin/words/{name} + Word JSON -> RET: OK 200 + Word JSON / ERR 404</li>
+   * </ul>
+   */
+  def getRoutes: HttpRoutes[IO] = {
+    val service: WordService = new WordService(wordDB)
+    val dsl = Http4sDsl[IO]
+    import dsl._
+    implicit val wordDecoder: EntityDecoder[IO, Word] = jsonOf[IO, Word]
+    HttpRoutes.of[IO] {
+      case GET -> Root / "words" :? OptionalCategoryParamMatcher(maybeCategory) =>
+        service.getWords(maybeCategory)
+      case request@POST -> Root / "words" =>
+        for {
+          newWord <- request.as[Word]
+          response <- service.addWord(newWord)
+        } yield response
+      case request@PUT -> Root / "words" / name =>
+        for {
+          newWord <- request.as[Word]
+          response <- service.updateWord(name, newWord)
+        } yield response
+      case DELETE -> Root / "words" / name =>
+        service.deleteWord(name)
+    }
+  }
+}

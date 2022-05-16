fadaf2c938f8a7d64caba8cb2ed73f5fba95445e
Piotr Ponikowski <piopon.github@gmail.com>
2022-05-16 14:07:46 +0200

    added new QuizDao class with round encoder getter

---
+++ backend/src/main/scala/pl/smtc/smartwords/utilities/QuizDao.scala
@@ -0,0 +1,11 @@
+package pl.smtc.smartwords.utilities
+
+import io.circe._
+import io.circe.literal._
+import pl.smtc.smartwords.model._
+
+object QuizDao {
+  def getRoundEncoder: Encoder[Round] = Encoder.instance {
+    (round: Round) => json"""{"word": ${round.word.name}, "options": ${round.options}}"""
+  }
+}

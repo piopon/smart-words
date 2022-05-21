909d199969db48a82c8c4a3131484057f0a067c1
Piotr Ponikowski <piopon.github@gmail.com>
2022-05-21 20:55:04 +0200

    added word and quiz dao code/methods documentation

--- backend/src/main/scala/pl/smtc/smartwords/dao/QuizDao.scala
+++ backend/src/main/scala/pl/smtc/smartwords/dao/QuizDao.scala
@@ -5,6 +5,11 @@ import io.circe.literal._
 import pl.smtc.smartwords.model._
 
 object QuizDao {
+
+  /**
+   * Method used to receive round encoder (to JSON)
+   * @return round object encoder
+   */
   def getRoundEncoder: Encoder[Round] = Encoder.instance {
     (round: Round) => json"""{"word": ${round.word.name}, "options": ${round.options}}"""
   }

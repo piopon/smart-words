39a8dbddda7566c0a30379fe37a2d14c26c6a01a
Piotr Ponikowski <piopon.github@gmail.com>
2022-05-23 22:57:06 +0200

    added new request events listener with readyState and status check

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,4 +1,13 @@
 const request = new XMLHttpRequest();
 
+request.addEventListener('readystatechange', () => {
+    if (request.DONE !== request.readyState) return;
+    if (request.status === 200) {
+        console.log(request.responseText);
+    } else {
+        console.log('ERROR');
+    }
+});
+
 request.open('GET', 'http://localhost:1234/words');
 request.send();

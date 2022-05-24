3275fc9342490a9c6e9cb99e13bd01593660c81c
Piotr Ponikowski <piopon.github@gmail.com>
2022-05-25 00:07:44 +0200

    updated table body content with service request response

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -19,6 +19,17 @@ getWords((err, data) => {
     if (err) {
         console.log('ERROR: ' + err);
     } else {
-        console.log(data);
+        const words = data.map((item) => {
+            return `<tr>
+                        <td>${item.name}</td>
+                        <td>
+                            <button class="buttonEdit">EDIT</button>
+                            <button class="buttonDelete">DELETE</button>
+                        </td>
+                        <td>${item.category}</td>
+                        <td>${item.description}</td>
+                    </tr>`;
+        }).join("");
+        document.querySelector('tbody').innerHTML = words;
     }
 })

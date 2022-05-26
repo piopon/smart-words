f0fcc08e85af92bf74bee08cfd93490851f723a9
Piotr Ponikowski <piopon.github@gmail.com>
2022-05-26 22:59:09 +0200

    removed get words method from script.js + added new API script to index.html

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,20 +1,3 @@
-const URL = 'http://localhost:1234/';
-
-const getWords = (callback) => {
-    const request = new XMLHttpRequest();
-
-    request.addEventListener('readystatechange', () => {
-        if (request.DONE !== request.readyState) return;
-        if (request.status === 200) {
-            callback(undefined, JSON.parse(request.responseText));
-        } else {
-            callback('cannot get words [' + request.status + ']', undefined);
-        }
-    });
-    request.open('GET', URL + 'words');
-    request.send();
-};
-
 getWords((err, data) => {
     if (err) {
         console.log('ERROR: ' + err);

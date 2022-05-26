73693b40aa95466ba7b04a1a8fac7d95beb64bf2
Piotr Ponikowski <piopon.github@gmail.com>
2022-05-26 22:58:28 +0200

    added new API words JS script file

---
+++ frontend/js/api-words.js
@@ -0,0 +1,16 @@
+const URL = 'http://localhost:1234/';
+
+const getWords = (callback) => {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('readystatechange', () => {
+        if (request.DONE !== request.readyState) return;
+        if (request.status === 200) {
+            callback(undefined, JSON.parse(request.responseText));
+        } else {
+            callback('cannot get words [' + request.status + ']', undefined);
+        }
+    });
+    request.open('GET', URL + 'words');
+    request.send();
+};

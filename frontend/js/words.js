ae82b0d6c31cbfc05e3ff2aff14825a176b1da7f
Piotr Ponikowski <piopon.github@gmail.com>
2022-05-31 22:17:07 +0200

    moved getWords on words.html load to a method

--- frontend/js/words.js
+++ frontend/js/words.js
@@ -16,22 +16,26 @@ function removeWord(name) {
     console.log("delete: " + name);
 }
 
+function loadWords() {
+    getWords((err, data) => {
+        if (err) {
+            console.log('ERROR: ' + err);
+        } else {
+            const words = data.map((item) => {
+                return `<tr>
+                            <td>${item.name}</td>
+                            <td>
+                                <button class="btn-edit" onclick="editWord('${item.name}')">EDIT</button>
+                                <button class="btn-delete" onclick="removeWord('${item.name}')">DELETE</button>
+                            </td>
+                            <td>${item.category}</td>
+                            <td>${item.description}</td>
+                        </tr>`;
+            }).join("");
+            document.querySelector('tbody').innerHTML = words;
+        }
+    });
+}
+
 // called on words.html site load
-getWords((err, data) => {
-    if (err) {
-        console.log('ERROR: ' + err);
-    } else {
-        const words = data.map((item) => {
-            return `<tr>
-                        <td>${item.name}</td>
-                        <td>
-                            <button class="btn-edit" onclick="editWord('${item.name}')">EDIT</button>
-                            <button class="btn-delete" onclick="removeWord('${item.name}')">DELETE</button>
-                        </td>
-                        <td>${item.category}</td>
-                        <td>${item.description}</td>
-                    </tr>`;
-        }).join("");
-        document.querySelector('tbody').innerHTML = words;
-    }
-})
+loadWords();

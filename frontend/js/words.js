e47cf244b314ecb921a077cb1a4d370530ef9889
Piotr Ponikowski <piopon.github@gmail.com>
2022-06-04 16:38:59 +0200

    reformatted words.js document

--- frontend/js/words.js
+++ frontend/js/words.js
@@ -4,14 +4,13 @@
  * @param {String} name word name to be edited
  */
 function editWord(name, category, definition) {
-    // update UI elements text
-    document.getElementById('word-edit-title').innerHTML = 'Edit word:';
-    document.getElementById('word-name').value = name;
-    document.getElementById('word-cat').value = category;
-    document.getElementById('word-def').innerHTML = definition;
-    document.getElementById('btn-word-edit-accept').value = 'edit';
-    // show edit form
-
+  // update UI elements text
+  document.getElementById("word-edit-title").innerHTML = "Edit word:";
+  document.getElementById("word-name").value = name;
+  document.getElementById("word-cat").value = category;
+  document.getElementById("word-def").innerHTML = definition;
+  document.getElementById("btn-word-edit-accept").value = "edit";
+  // show edit form
 }
 
 /**
@@ -20,39 +19,41 @@ function editWord(name, category, definition) {
  * @param {String} name word name to be deleted
  */
 function removeWord(name) {
-    deleteWord(name, (err, data) => {
-        if (err) {
-            console.log('ERROR: ' + err);
-        } else {
-            loadWords();
-        }
-    });
+  deleteWord(name, (err, data) => {
+    if (err) {
+      console.log("ERROR: " + err);
+    } else {
+      loadWords();
+    }
+  });
 }
 
 function getWordTableRow(item) {
-    return `<tr>
-                <td>${item.name}</td>
-                <td>
-                    <button class="btn-edit" onclick="editWord('${item.name}', '${item.category}', '${item.description}')">
-                        EDIT
-                    </button>
-                    <button class="btn-delete" onclick="removeWord('${item.name}')">
-                        DELETE
-                    </button>
-                </td>
-                <td>${item.category}</td>
-                <td>${item.description}</td>
-            </tr>`;
+  return `<tr>
+            <td>${item.name}</td>
+            <td>
+              <button class="btn-edit" onclick="editWord('${item.name}', '${item.category}', '${item.description}')">
+                  EDIT
+              </button>
+              <button class="btn-delete" onclick="removeWord('${item.name}')">
+                  DELETE
+              </button>
+            </td>
+            <td>${item.category}</td>
+            <td>${item.description}</td>
+          </tr>`;
 }
 
 function loadWords() {
-    getWords((err, data) => {
-        if (err) {
-            console.log('ERROR: ' + err);
-        } else {
-            document.querySelector('tbody').innerHTML = Object.values(data).map((item) => getWordTableRow(item)).join("");
-        }
-    });
+  getWords((err, data) => {
+    if (err) {
+      console.log("ERROR: " + err);
+    } else {
+      document.querySelector("tbody").innerHTML = Object.values(data)
+        .map((item) => getWordTableRow(item))
+        .join("");
+    }
+  });
 }
 
 // called on words.html site load

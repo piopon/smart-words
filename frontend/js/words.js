5d7debf43caa54994b644805be66a2b89e85a030
Piotr Ponikowski <piopon.github@gmail.com>
2022-06-04 16:43:01 +0200

    added form mode variable + addWord method and updated HTML reference

--- frontend/js/words.js
+++ frontend/js/words.js
@@ -1,11 +1,28 @@
+const FORM_MODE_ADD = 0;
+const FORM_MODE_EDIT = 1;
+var wordFormMode = FORM_MODE_ADD;
+
+/**
+ * Method used to add new word and show edit word UI form with empty word data.
+ */
+function addWord() {
+  wordFormMode = FORM_MODE_ADD;
+  document.getElementById("word-form-title").innerHTML = "Add word:";
+  document.getElementById("word-name").value = "";
+  document.getElementById("word-cat").value = "";
+  document.getElementById("word-def").innerHTML = "";
+  document.getElementById("btn-word-edit-accept").value = "add";
+  document.getElementById("word-form").className = "form-visible";
+}
+
 /**
  * Method used to edit word with specified name.
  *
  * @param {String} name word name to be edited
  */
 function editWord(name, category, definition) {
-  // update UI elements text
-  document.getElementById("word-edit-title").innerHTML = "Edit word:";
+  wordFormMode = FORM_MODE_EDIT;
+  document.getElementById("word-form-title").innerHTML = "Edit word:";
   document.getElementById("word-name").value = name;
   document.getElementById("word-cat").value = category;
   document.getElementById("word-def").innerHTML = definition;

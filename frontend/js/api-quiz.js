665c7e99990ae691c05f054db23ac5bf8e2dd4f3
Piotr Ponikowski <piopon.github@gmail.com>
2022-06-10 00:31:13 +0200

    added new getQuestionNo method in quiz API

--- frontend/js/api-quiz.js
+++ frontend/js/api-quiz.js
@@ -13,3 +13,17 @@ const postQuizStart = (questionsNo, callback) => {
   postRequest.open("POST", URL + "quiz/start?size=" + questionsNo);
   postRequest.send();
 };
+
+const getQuestionNo = (quizID, questionNo, callback) => {
+  const getRequest = new XMLHttpRequest();
+  getRequest.addEventListener("readystatechange", () => {
+    if (getRequest.DONE !== getRequest.readyState) return;
+    if (getRequest.status === 200) {
+      callback(undefined, JSON.parse(getRequest.responseText));
+    } else {
+      callback("cannot get question no " + questionNo + " [" + getRequest.status + "]", undefined);
+    }
+  });
+  getRequest.open("GET", URL + "quiz/" + quizID + "/question/" + questionNo);
+  getRequest.send();
+}

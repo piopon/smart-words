24a5339cfc1582a0397e6eda9a5e7fdbbcf34e01
Piotr Ponikowski <piopon.github@gmail.com>
2022-06-11 23:10:19 +0200

    added requestQUestioNo method with get question API call

--- frontend/js/quiz.js
+++ frontend/js/quiz.js
@@ -10,6 +10,19 @@ function startQuiz() {
     } else {
       console.log(data);
       quizID = data;
+      currentQuestionNo = 0;
+      requestQuestionNo(currentQuestionNo);
     }
   });
 }
+
+function requestQuestionNo(number) {
+  getQuestionNo(quizID, number, (err, data) => {
+    if (err) {
+      console.log("ERROR: " + err);
+    } else {
+      console.log(data);
+    }
+  });
+}
+

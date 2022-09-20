1d3aedf5fec50841d1d806b0e12d0d83862b4c1a
Piotr Ponikowski <piopon.github@gmail.com>
2022-09-21 00:11:29 +0200

    added dictionary field to Word model class for storing source dictionary file name

--- backend/src/main/scala/pl/smtc/smartwords/model/Word.scala
+++ backend/src/main/scala/pl/smtc/smartwords/model/Word.scala
@@ -5,5 +5,6 @@ package pl.smtc.smartwords.model
  * @param name smart word title/name
  * @param category word category
  * @param definition word correct definition
+ * @param dictionary word source dictionary file
  */
-case class Word(name: String, category: Category.Value, definition: List[String])
+case class Word(name: String, category: Category.Value, definition: List[String], var dictionary: String)

61c801a9fcb2e3b0cc822bf7d501837a99cf75ae
Piotr Ponikowski <piopon.github@gmail.com>
2022-10-03 23:03:36 +0200

    updated main run method to create server and display message when ok

--- backend/src/main/scala/pl/smtc/smartwords/SmartWordsApp.scala
+++ backend/src/main/scala/pl/smtc/smartwords/SmartWordsApp.scala
@@ -19,14 +19,16 @@ object SmartWordsApp extends IOApp {
     val wordController: WordController = new WordController(wordDB)
     val quizController: QuizController = new QuizController(wordDB)
 
-    if (wordDB.loadDatabase()) {
-      val config = CORSConfig(anyOrigin = true, allowCredentials = true, 1.day.toSeconds, anyMethod = true)
-      val apis = Router(
-        "/quiz" -> CORS(quizController.getRoutes, config),
-        "/words" -> CORS(wordController.getRoutes, config)
-      ).orNotFound
-
-      EmberServerBuilder.default[IO]
+    if (!wordDB.loadDatabase()) {
+      return IO.canceled.as(ExitCode.Error)
+    }
+    val config = CORSConfig(anyOrigin = true, allowCredentials = true, 1.day.toSeconds, anyMethod = true)
+    val apis = Router(
+      "/quiz" -> CORS(quizController.getRoutes, config),
+      "/words" -> CORS(wordController.getRoutes, config)
+    ).orNotFound
+    for {
+      server <- EmberServerBuilder.default[IO]
         .withHost(ipv4"0.0.0.0")
         .withPort(port"1234")
         .withHttpApp(apis)
@@ -35,10 +37,10 @@ object SmartWordsApp extends IOApp {
             .as(Response(status = Status.InternalServerError))
         }
         .build
-        .use(_ => IO.never)
-        .as(ExitCode.Success)
-    } else {
-      IO.canceled.as(ExitCode.Error)
-    }
-  }
+    } yield server
+  }.use(server => {
+    val serverAddress = server.address.getAddress.getHostAddress
+    val serverPort = server.address.getPort
+    IO.delay(println(s"Service started: IPv6=$serverAddress, port=$serverPort")) >> IO.never.as(ExitCode.Success)
+  })
 }

46d5f1b8805b8f64d2ee733c22b8aaabdfedfd33
Piotr Ponikowski <piopon.github@gmail.com>
2022-10-05 19:00:29 +0200

    removed unnecessary quiz-related classes (controller, service, model, dao)

--- backend/service-word/src/main/scala/pl/smtc/smartwords/model/Quiz.scala
+++
@@ -1,8 +0,0 @@
-package pl.smtc.smartwords.model
-
-/**
- * Model class representing a complete quiz containing several rounds (smart words questions)
- * @param rounds a collection of rounds/questions
- * @param score current correct answers counter
- */
-case class Quiz(rounds: List[Round], score: Int)

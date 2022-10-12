cf9f9b69d6478fe1bc003563f375c1ce74391971
Piotr Ponikowski <piopon.github@gmail.com>
2022-10-12 23:39:45 +0200

    updated word controller docs with random flag

--- backend/service-word/src/main/scala/pl/smtc/smartwords/controller/WordController.scala
+++ backend/service-word/src/main/scala/pl/smtc/smartwords/controller/WordController.scala
@@ -27,6 +27,7 @@ class WordController(wordDB: WordDatabase) {
    *  <li>Receive all words: <u>GET</u> /words -> RET: OK 200 + ALL WORDS JSON / ERR 500</li>
    *  <li>Receive specified number of words: <u>GET</u> /words?size=no -> RET: OK 200 + Word JSON / ERR 500</li>
    *  <li>Receive category-specific words: <u>GET</u> /words?cat=adj -> RET: OK 200 + Word JSON / ERR 500</li>
+   *  <li>Receive words in random order: <u>GET</u> /words?random=bool -> RET: OK 200 + Word JSON / ERR 500</li>
    *  <li>Add a new word: <u>POST</u> /words + Word JSON -> RET: OK 200 / ERR 500</li>
    *  <li>Delete word: <u>DELETE</u> /words/{name} -> RET: OK 200 / ERR 404</li>
    *  <li>Update word: <u>PUT</u> /words/{name} + Word JSON -> RET: OK 200 + Word JSON / ERR 404</li>

23bd74afdbb34c55f6041f3fb9da636757bd5652
Piotr Ponikowski <piopon.github@gmail.com>
2022-10-12 23:47:58 +0200

    updated request variables to use address class field & resolve URI

--- backend/service-quiz/src/main/scala/pl/smtc/smartwords/client/WordService.scala
+++ backend/service-quiz/src/main/scala/pl/smtc/smartwords/client/WordService.scala
@@ -13,6 +13,8 @@ import pl.smtc.smartwords.model._
 
 class WordService {
 
+  val address: Uri = uri"http://localhost:1111"
+
   implicit val WordsDecoder: EntityDecoder[IO, List[Word]] = jsonOf[IO, List[Word]]
 
   /**
@@ -21,7 +23,9 @@ class WordService {
    */
   def getRandomWord: Word = {
     var word: Word = null
-    val getWordRequest = GET(uri"http://localhost:1111/words?size=1&random=true")
+    val getWordRequest = GET(address.withPath(path"words")
+                                    .withQueryParam("size", "1")
+                                    .withQueryParam("random", "true"))
     EmberClientBuilder.default[IO].build.use(client =>
       client.expect[List[Word]](getWordRequest).map(response => word = response.head)
     ).unsafeRunSync()
@@ -35,7 +39,8 @@ class WordService {
    */
   def getWordsByCategory(category: String): List[Word] = {
     var categoryWords: List[Word] = List()
-    val wordServiceRequest = GET(uri"http://localhost:1111/words".withQueryParam("cat", category))
+    val wordServiceRequest = GET(address.withPath(path"words")
+                                        .withQueryParam("cat", category))
     EmberClientBuilder.default[IO].build.use(client =>
       client.expect[List[Word]](wordServiceRequest).map(response => categoryWords = response)
     ).unsafeRunSync()

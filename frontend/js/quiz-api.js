c2a3e05f7d150c15b2850711be084e2cb2a31ba3
Piotr Ponikowski <piopon.github@gmail.com>
2022-11-29 21:17:41 +0100

    added get quiz modes method to quiz API JS file

--- frontend/js/quiz-api.js
+++ frontend/js/quiz-api.js
@@ -86,3 +86,23 @@ const getQuizStop = (quizID, callback) => {
   getRequest.open("GET", URL + "quiz/" + quizID + "/stop");
   getRequest.send();
 };
+
+/**
+ * Method used to send a request to the quiz service to receive all quiz modes
+ *
+ * @param {Function} callback function to be invoked when request is completed.
+ *                            It should contain 2 parameters: error string and data object.
+ */
+ const getQuizModes = (callback) => {
+  const getRequest = new XMLHttpRequest();
+  getRequest.addEventListener("readystatechange", () => {
+    if (getRequest.DONE !== getRequest.readyState) return;
+    if (getRequest.status === 200) {
+      callback(undefined, JSON.parse(getRequest.responseText));
+    } else {
+      callback("cannot get quiz modes " + " [" + getRequest.status + "]", undefined);
+    }
+  });
+  getRequest.open("GET", URL + "modes");
+  getRequest.send();
+};

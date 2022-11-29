5b1205aee28e97c0975011d8b4845f45f7f48c7a
Piotr Ponikowski <piopon.github@gmail.com>
2022-11-30 00:08:30 +0100

    new get mode settings method (currently containing hardcoded data)

--- frontend/js/quiz-mode.js
+++ frontend/js/quiz-mode.js
@@ -22,6 +22,21 @@ function getModeDescription(description) {
           </div>`;
 }
 
+function getModeSettings(settings) {
+  return `<p class="mode-section-label">settings:</p>
+          <div>
+            <label for="setting-1">select language:</label>
+            <img src="images/languages/pl.png"/>
+            <img src="images/languages/en.png"/>
+            <img src="images/languages/fr.png"/>
+            <img src="images/languages/de.png"/>
+          </div>
+          <div>
+            <label for="setting-2">how many questions:</label>
+            <input type="number" id="setting-2" value="10" min="5" max="25" />
+          </div>`;
+}
+
 function getModeControls() {
   return `<div id="quiz-mode-tile-controls">
             <button id="quiz-mode-controls-start" class="dynamic-border" onclick="startQuiz()">start</button>

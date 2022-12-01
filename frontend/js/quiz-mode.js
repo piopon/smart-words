a36e3ce06ed4fb51f0b1b381c0e68f2e8b3ce352
Piotr Ponikowski <piopon.github@gmail.com>
2022-12-02 00:23:24 +0100

    added descriptions to all methods in quiz-mode.js

--- frontend/js/quiz-mode.js
+++ frontend/js/quiz-mode.js
@@ -15,6 +15,12 @@ function showQuizModes() {
   });
 }
 
+/**
+ * Method used to receive complete single quiz mode HTML code
+ *
+ * @param {Object} mode containig all data needed to display quiz mode
+ * @returns HTML content with single quiz mode
+ */
 function getModeHtml(mode) {
   return `<div id="quiz-mode-tile" class="tile-size-big tile-color-primary">
             ${getTitleHtml(mode.name)}
@@ -28,10 +34,22 @@ function getModeHtml(mode) {
           </div>`;
 }
 
+/**
+ * Method used to receive mode title HTML code
+ *
+ * @param {String} title of current quiz mode
+ * @returns HTML content with mode title
+ */
 function getTitleHtml(title) {
   return `<div id="quiz-mode-tile-title">${title}</div>`;
 }
 
+/**
+ * Method used to receive mode description HTML code
+ *
+ * @param {String} description of current quiz mode
+ * @returns HTML content with mode description
+ */
 function getDescriptionHtml(description) {
   return `<div id="quiz-mode-content-description">
             <p class="mode-section-label">decription:</p>
@@ -39,6 +57,12 @@ function getDescriptionHtml(description) {
           </div>`;
 }
 
+/**
+ * Method used to receive all mode settings HTML code
+ *
+ * @param {Object} settings list of all settings related to current mode
+ * @returns HTML content with all mode settings
+ */
 function getSettingsHtml(settings) {
   let allSettingsHtml = Object.values(settings)
     .map((item) => getSettingHtml(item))
@@ -46,6 +70,12 @@ function getSettingsHtml(settings) {
   return `<p class="mode-section-label">settings:</p>` + allSettingsHtml;
 }
 
+/**
+ * Method used to receive mode single setting HTML code
+ *
+ * @param {Object} setting data with specified type, label and details
+ * @returns HTML content with single mode setting
+ */
 function getSettingHtml(setting) {
   switch (setting.type) {
     case "languages":
@@ -57,6 +87,13 @@ function getSettingHtml(setting) {
   }
 }
 
+/**
+ * Method used to receive mode supported languages setting HTML code
+ *
+ * @param {String} label ontaining information about languages setting
+ * @param {String} details containing short names of supported languages
+ * @returns HTML content with mode languages setting
+ */
 function getLanguagesHtml(label, details) {
   let supportedLanguages = details
     .split(" ")
@@ -68,10 +105,23 @@ function getLanguagesHtml(label, details) {
           </div>`;
 }
 
+/**
+ * Method used to receive specified language flag HTML code
+ *
+ * @param {String} language flag name representing specific image in languages resources
+ * @returns HTML content with specified language icon
+ */
 function getLanguageHtml(language) {
   return `<img class="language-flag" src="images/languages/${language}-24.png"/>`;
 }
 
+/**
+ * Method used to receive mode total questions number setting HTML code
+ *
+ * @param {String} label containing information about questions number setting
+ * @param {String} details containing questions number value information
+ * @returns HTML content with mode questions number setting
+ */
 function getQuestionsHtml(label, details) {
   return `<div class="quiz-mode-setting-container">
             <label for="quiz-mode-settings-question-no">${label}</label>
@@ -79,6 +129,11 @@ function getQuestionsHtml(label, details) {
           </div>`;
 }
 
+/**
+ * Method used to create mode controls HTML code
+ *
+ * @returns HTML content with mode controls
+ */
 function getControlsHtml() {
   return `<div id="quiz-mode-tile-controls">
             <button id="quiz-mode-controls-start" class="dynamic-border" onclick="startQuiz()">start</button>

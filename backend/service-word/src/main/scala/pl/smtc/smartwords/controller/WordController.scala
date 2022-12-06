1e30a6abb9270bd9fcabc322d38ddd94372feebe
Piotr Ponikowski <piopon.github@gmail.com>
2022-12-06 01:29:51 +0100

    updated word controller docs: matching API sequence to methods

--- backend/service-word/src/main/scala/pl/smtc/smartwords/controller/WordController.scala
+++ backend/service-word/src/main/scala/pl/smtc/smartwords/controller/WordController.scala
@@ -31,8 +31,8 @@ class WordController(wordDB: WordDatabase) {
    *  <li>Receive category-specific words: <u>GET</u> /words?cat=adj -> RET: OK 200 + Word JSON / ERR 500</li>
    *  <li>Receive words in random order: <u>GET</u> /words?random=bool -> RET: OK 200 + Word JSON / ERR 500</li>
    *  <li>Add a new word: <u>POST</u> /words + Word JSON -> RET: OK 200 / ERR 500</li>
-   *  <li>Delete word: <u>DELETE</u> /words/{name} -> RET: OK 200 / ERR 404</li>
    *  <li>Update word: <u>PUT</u> /words/{name} + Word JSON -> RET: OK 200 + Word JSON / ERR 404</li>
+   *  <li>Delete word: <u>DELETE</u> /words/{name} -> RET: OK 200 / ERR 404</li>
    * </ul>
    */
   def getRoutes: HttpRoutes[IO] = {

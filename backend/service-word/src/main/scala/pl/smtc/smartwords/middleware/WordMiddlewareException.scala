e8fedbc2403825345f32f44c4ef28ac61b1fa5a8
Piotr Ponikowski <piopon.github@gmail.com>
2023-01-08 00:12:48 +0100

    added new middleware package + exception class

---
+++ backend/service-word/src/main/scala/pl/smtc/smartwords/middleware/WordMiddlewareException.scala
@@ -0,0 +1,17 @@
+package pl.smtc.smartwords.middleware
+
+class WordMiddlewareException(msg: String) extends Exception(msg) {
+  def this(msg: String, cause: Throwable) = {
+    this(msg)
+    initCause(cause)
+  }
+
+  def this(cause: Throwable) = {
+    this(Option(cause).map(_.toString).orNull)
+    initCause(cause)
+  }
+
+  def this() = {
+    this(null: String)
+  }
+}

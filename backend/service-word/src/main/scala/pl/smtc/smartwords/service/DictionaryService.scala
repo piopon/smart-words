0070d777a97b5caef67660f7dddb23cb8807975e
Piotr Ponikowski <piopon.github@gmail.com>
2023-01-14 00:58:17 +0100

    added toJson method in dictionary service class

--- backend/service-word/src/main/scala/pl/smtc/smartwords/service/DictionaryService.scala
+++ backend/service-word/src/main/scala/pl/smtc/smartwords/service/DictionaryService.scala
@@ -1,8 +1,21 @@
 package pl.smtc.smartwords.service
 
+import cats.effect._
+import io.circe._
+import io.circe.literal._
+import org.http4s._
 import pl.smtc.smartwords.database._
+import pl.smtc.smartwords.model._
 
 class DictionaryService(wordDB: WordDatabase) {
 
   def getDictionariesInfo() = ???
+
+  def toJson(dictionary: Dictionary): Json = {
+    json"""{
+          "game": ${dictionary.game},
+          "mode": ${dictionary.mode},
+          "language": ${dictionary.language}
+        }"""
+  }
 }

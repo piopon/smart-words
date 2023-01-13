d63ce998b22a36c6223fd797249c6c5a15628f39
Piotr Ponikowski <piopon.github@gmail.com>
2023-01-14 00:59:27 +0100

    updated logic to retrieve dictionaries info

--- backend/service-word/src/main/scala/pl/smtc/smartwords/service/DictionaryService.scala
+++ backend/service-word/src/main/scala/pl/smtc/smartwords/service/DictionaryService.scala
@@ -3,13 +3,20 @@ package pl.smtc.smartwords.service
 import cats.effect._
 import io.circe._
 import io.circe.literal._
+import io.circe.syntax._
 import org.http4s._
+import org.http4s.circe._
+import org.http4s.dsl.io._
 import pl.smtc.smartwords.database._
 import pl.smtc.smartwords.model._
 
 class DictionaryService(wordDB: WordDatabase) {
 
-  def getDictionariesInfo() = ???
+  def getDictionariesInfo(): IO[Response[IO]] = {
+    val availableDictionaries: Set[Dictionary] = wordDB.getWords.groupBy((word: Word) => word.dictionary).keySet
+    val rawJson: Set[Json] = availableDictionaries.map(d => toJson(d))
+    Ok(rawJson.asJson)
+  }
 
   def toJson(dictionary: Dictionary): Json = {
     json"""{

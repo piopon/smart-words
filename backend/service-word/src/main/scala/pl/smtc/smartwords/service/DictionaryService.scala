faddc8d363ecd0fea7ab8c98cd8ad228b72fcb50
Piotr Ponikowski <piopon.github@gmail.com>
2023-01-14 01:13:27 +0100

    significantly simplified getDictionaries logic in DictionaryService

--- backend/service-word/src/main/scala/pl/smtc/smartwords/service/DictionaryService.scala
+++ backend/service-word/src/main/scala/pl/smtc/smartwords/service/DictionaryService.scala
@@ -1,19 +1,15 @@
 package pl.smtc.smartwords.service
 
 import cats.effect._
-import io.circe._
 import io.circe.syntax._
 import org.http4s._
 import org.http4s.circe._
 import org.http4s.dsl.io._
 import pl.smtc.smartwords.database._
-import pl.smtc.smartwords.model._
 
 class DictionaryService(wordDB: WordDatabase) {
 
   def getDictionaries: IO[Response[IO]] = {
-    val availableDictionaries: Set[Dictionary] = wordDB.getWords.groupBy((word: Word) => word.dictionary).keySet
-    val rawJson: Set[Json] = availableDictionaries.map(d => d.toJson)
-    Ok(rawJson.asJson)
+    Ok(wordDB.getWords.groupBy(_.dictionary).keySet.map(_.toJson).asJson)
   }
 }

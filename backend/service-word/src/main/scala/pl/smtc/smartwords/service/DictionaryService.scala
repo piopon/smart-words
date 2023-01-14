0bf50dae40ea998873dcd1529e4ee466fa310ad1
Piotr Ponikowski <piopon.github@gmail.com>
2023-01-14 01:18:53 +0100

    used dictionary encoder in service class and shortened getDictionaries logic even more

--- backend/service-word/src/main/scala/pl/smtc/smartwords/service/DictionaryService.scala
+++ backend/service-word/src/main/scala/pl/smtc/smartwords/service/DictionaryService.scala
@@ -1,15 +1,20 @@
 package pl.smtc.smartwords.service
 
 import cats.effect._
+import io.circe._
 import io.circe.syntax._
 import org.http4s._
 import org.http4s.circe._
 import org.http4s.dsl.io._
+import pl.smtc.smartwords.dao._
 import pl.smtc.smartwords.database._
+import pl.smtc.smartwords.model._
 
 class DictionaryService(wordDB: WordDatabase) {
 
+  implicit val DictionaryEncoder: Encoder[Dictionary] = DictionaryDao.getDictionaryEncoder
+
   def getDictionaries: IO[Response[IO]] = {
-    Ok(wordDB.getWords.groupBy(_.dictionary).keySet.map(_.toJson).asJson)
+    Ok(wordDB.getWords.groupBy(_.dictionary).keySet.asJson)
   }
 }

b366c3e76a5ad45639de52e2afb5c37739a597d6
Piotr Ponikowski <piopon.github@gmail.com>
2023-01-16 23:33:42 +0100

    added log-only methods connected with every selector onchange attribute

--- frontend/js/words-dictionary.js
+++ frontend/js/words-dictionary.js
@@ -1,2 +1,14 @@
 var selectedMode = "0";
 var selectedLanguage = "pl";
+
+function gameChanged() {
+    console.log("Game changed");
+}
+
+function modeChanged() {
+    console.log("Mode changed");
+}
+
+function languageChanged() {
+    console.log("Language changed");
+}

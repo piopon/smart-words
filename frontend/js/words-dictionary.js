cea5959f62a03a3b6e099d07e6e7e61c4ac1abfd
Piotr Ponikowski <piopon.github@gmail.com>
2023-01-16 23:59:59 +0100

    added dictionary elements sets + filling each selector individually

--- frontend/js/words-dictionary.js
+++ frontend/js/words-dictionary.js
@@ -1,14 +1,33 @@
 var selectedMode = "0";
 var selectedLanguage = "pl";
+var availableGames = new Set();
+var availableModes = new Set();
+var availableLangs = new Set();
 
 function fillDictionarySelectors() {
   getDictionaries((err, data) => {
     if (err) {
       console.log("ERROR " + err.status + ": " + err.message);
     } else {
-      console.log(data);
+      Object.values(data).forEach(dictionary => {
+        availableGames.add(dictionary.game);
+        availableModes.add(dictionary.mode);
+        availableLangs.add(dictionary.language);
+      });
+      fillSelector("game", Array.from(availableGames.values()));
+      fillSelector("mode", Array.from(availableModes.values()));
+      fillSelector("language", Array.from(availableLangs.values()));
     }
   });
+}
+
+function fillSelector(type, values) {
+    let selector = document.getElementById(`dictionary-selector-${type}`);
+    let optionsHtml = `<option value="" default selected hidden>select ${type}</option>`;
+    values.forEach(value => {
+        optionsHtml += `<option value="${value}">${value}</option>`;
+    })
+    selector.innerHTML = optionsHtml;
 }
 
 function gameChanged() {

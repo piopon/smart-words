a616d2f1ff220b254ebda02d1f5ffe79f923975c
Piotr Ponikowski <piopon.github@gmail.com>
2023-01-26 00:46:26 +0100

    refactored selectTab to be more precise and operate on classes

--- frontend/js/settings.js
+++ frontend/js/settings.js
@@ -1,15 +1,13 @@
-function selectTab(event, tabName) {
-  // This is to clear the previous clicked content.
+function selectTab(event, tabId) {
   let tabContent = document.getElementsByClassName("tab-content");
   for (i = 0; i < tabContent.length; i++) {
-    tabContent[i].style.display = "none";
+    tabContent[i].classList.remove("visible");
   }
-  // Set the tab to be "active".
+  document.getElementById(tabId).classList.add("visible");
+
   let tabItems = document.getElementsByClassName("tab-item");
   for (i = 0; i < tabItems.length; i++) {
-    tabItems[i].className = tabItems[i].className.replace(" active", "");
+      tabItems[i].className = tabItems[i].className.replace(" active", "");
   }
-  // Display the clicked tab and set it to active.
-  document.getElementById(tabName).style.display = "flex";
   event.currentTarget.className += " active";
 }

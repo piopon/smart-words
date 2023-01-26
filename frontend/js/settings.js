bd82328cfab4e2284258e83f47d202818e94b580
Piotr Ponikowski <piopon.github@gmail.com>
2023-01-27 00:49:13 +0100

    refactored selectTab() method to make it more readable

--- frontend/js/settings.js
+++ frontend/js/settings.js
@@ -1,13 +1,12 @@
 function selectTab(event, tabId) {
-  let tabContent = document.getElementsByClassName("tab-content");
-  for (i = 0; i < tabContent.length; i++) {
-    tabContent[i].classList.remove("visible");
-  }
-  document.getElementById(tabId).classList.add("visible");
-
   let tabItems = document.getElementsByClassName("tab-item");
   for (i = 0; i < tabItems.length; i++) {
-      tabItems[i].className = tabItems[i].className.replace(" active", "");
+    tabItems[i].className = tabItems[i].className.replace(" active", "");
   }
   event.currentTarget.className += " active";
+
+  let tabContent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabContent.length; i++) {
+    tabContent[i].className = "tab-content" + (tabContent[i].id === tabId ? " visible" : "");
+  }
 }

3b2aa66a19aaea2b42b3bcee912a3fe322e561bf
Piotr Ponikowski <piopon.github@gmail.com>
2023-02-15 01:18:05 +0100

    added method description to method in general setting JS logic

--- frontend/js/settings.js
+++ frontend/js/settings.js
@@ -1,7 +1,16 @@
+/**
+ * Method used to initialize settings page (main entry point after loading settings.html)
+ */
 function initializeSettings() {
   initializeTabQuizModes();
 }
 
+/**
+ * Method used to update settings view after clicking on tabs navigation button
+ *
+ * @param {Object} event containing the currently pressed target
+ * @param {Integer} tabId the ID of the tab content which should be visible after selecting tab navigation button
+ */
 function selectTab(event, tabId) {
   let tabItems = document.getElementsByClassName("tab-item");
   for (i = 0; i < tabItems.length; i++) {

b644a76b284e0589137a549e7486d3b987c7b8f3
Piotr Ponikowski <piopon.github@gmail.com>
2023-03-09 00:37:11 +0100

    added mode decoder + entity decoder implicit declarations

--- backend/service-quiz/src/main/scala/pl/smtc/smartwords/controller/ModeController.scala
+++ backend/service-quiz/src/main/scala/pl/smtc/smartwords/controller/ModeController.scala
@@ -1,11 +1,17 @@
 package pl.smtc.smartwords.controller
 
 import cats.effect._
+import org.http4s.circe._
+import io.circe._
 import org.http4s._
 import org.http4s.dsl._
+import pl.smtc.smartwords.dao._
+import pl.smtc.smartwords.model._
 import pl.smtc.smartwords.service._
 
 class ModeController {
+
+  implicit val ModeDecoder: Decoder[Mode] = ModeDao.getModeDecoder
 
   /**
    * Routes (request -> response) for quiz modes
@@ -16,6 +22,7 @@ class ModeController {
   def getRoutes: HttpRoutes[IO] = {
     val service: ModeService = new ModeService()
     val dsl = Http4sDsl[IO]; import dsl._
+    implicit val modeDecoder: EntityDecoder[IO, Mode] = jsonOf[IO, Mode]
     HttpRoutes.of[IO] {
       case GET -> Root =>
         service.getQuizModes

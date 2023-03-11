c48269011669773585ba48202ce0f0fbb19efd7a
Piotr Ponikowski <piopon.github@gmail.com>
2023-03-11 17:59:50 +0100

    added mode database class with initialize available modes list

---
+++ backend/service-quiz/src/main/scala/pl/smtc/smartwords/database/ModeDatabase.scala
@@ -0,0 +1,37 @@
+package pl.smtc.smartwords.database
+
+import io.circe._
+import io.circe.parser._
+import pl.smtc.smartwords.dao._
+import pl.smtc.smartwords.model._
+
+import java.io.{BufferedInputStream, File, FileInputStream}
+import java.nio.file.{Path, Paths}
+import scala.collection.mutable.ListBuffer
+import scala.io.Source
+import scala.util.Using
+
+class ModeDatabase {
+
+  implicit val ModeDecoder: Decoder[Mode] = ModeDao.getModeDecoder
+
+  private val resourceDir: Path = Paths.get(getClass.getResource("/").toURI)
+  private val quizModesFile = "modes.json"
+
+  /**
+   * Method used to initialize quiz modes list with data from internal JSON file
+   * @return ListBuffer with supported quiz modes
+   */
+  def initializeModes(): ListBuffer[Mode] = {
+    val foundModes: ListBuffer[Mode] = new ListBuffer()
+    val modesFile = new File(resourceDir.resolve(quizModesFile).toString)
+    Using(new BufferedInputStream(new FileInputStream(modesFile))) { fileStream =>
+      val lines = Source.fromInputStream(fileStream).getLines.mkString.stripMargin
+      decode[List[Mode]](lines) match {
+        case Right(modes) => modes.foreach(mode => foundModes += mode)
+        case Left(fail) => println(s"Invalid modes file ${modesFile.getName}: ${fail.getMessage}")
+      }
+    }
+    foundModes
+  }
+}

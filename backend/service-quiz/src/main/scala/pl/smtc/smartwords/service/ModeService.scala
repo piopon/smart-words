2649cbc4d58656f928589e1e1f2cdff2838d8bda
Piotr Ponikowski <piopon.github@gmail.com>
2023-03-11 18:01:26 +0100

    updated ModeService class to use database + removed not needed logic

--- backend/service-quiz/src/main/scala/pl/smtc/smartwords/service/ModeService.scala
+++ backend/service-quiz/src/main/scala/pl/smtc/smartwords/service/ModeService.scala
@@ -3,28 +3,21 @@ package pl.smtc.smartwords.service
 import cats.effect._
 import io.circe._
 import io.circe.syntax._
-import io.circe.parser._
 import org.http4s._
 import org.http4s.circe._
 import org.http4s.dsl.io._
 import pl.smtc.smartwords.dao._
+import pl.smtc.smartwords.database._
 import pl.smtc.smartwords.model._
 
-import java.io.{BufferedInputStream, File, FileInputStream}
-import java.nio.file.{Path, Paths}
 import scala.collection.mutable.ListBuffer
-import scala.io.Source
-import scala.util.Using
 
 class ModeService {
 
-  implicit val ModeDecoder: Decoder[Mode] = ModeDao.getModeDecoder
   implicit val ModeEncoder: Encoder[Mode] = ModeDao.getModeEncoder
   implicit val SettingEncoder: Encoder[Setting] = SettingDao.getSettingEncoder
 
-  private val resourceDir: Path = Paths.get(getClass.getResource("/").toURI)
-  private val quizModesFile = "modes.json"
-  private val quizModes: ListBuffer[Mode] = initializeModes()
+  private val quizModes: ListBuffer[Mode] = new ModeDatabase().initializeModes()
 
   /**
    * Method used to receive the list of current quiz modes
@@ -69,22 +62,5 @@ class ModeService {
     mode.id = id
     quizModes.update(idPosition, mode)
     Ok(s"Updated quiz mode ID: $id")
-  }
-
-  /**
-   * Method used to initialize quiz modes list with data from internal JSON file
-   * @return ListBuffer with supported quiz modes
-   */
-  private def initializeModes(): ListBuffer[Mode] = {
-    val foundModes: ListBuffer[Mode] = new ListBuffer()
-    val modesFile = new File(resourceDir.resolve(quizModesFile).toString)
-    Using(new BufferedInputStream(new FileInputStream(modesFile))) { fileStream =>
-      val lines = Source.fromInputStream(fileStream).getLines.mkString.stripMargin
-      decode[List[Mode]](lines) match {
-        case Right(modes) => modes.foreach(mode => foundModes += mode)
-        case Left(fail) => println(s"Invalid modes file ${modesFile.getName}: ${fail.getMessage}")
-      }
-    }
-    foundModes
   }
 }

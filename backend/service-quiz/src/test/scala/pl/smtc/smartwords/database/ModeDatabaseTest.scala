a6d4fa3af883dc825a059c362ef4471637bf0c3a
Piotr Ponikowski <piopon.github@gmail.com>
2023-04-30 17:23:40 +0200

    added all the CRUD operation tests to ModeDatabase class

--- backend/service-quiz/src/test/scala/pl/smtc/smartwords/database/ModeDatabaseTest.scala
+++ backend/service-quiz/src/test/scala/pl/smtc/smartwords/database/ModeDatabaseTest.scala
@@ -1,6 +1,7 @@
 package pl.smtc.smartwords.database
 
 import org.scalatest.funsuite.AnyFunSuite
+import pl.smtc.smartwords.model.Mode
 
 class ModeDatabaseTest extends AnyFunSuite {
 
@@ -11,5 +12,42 @@ class ModeDatabaseTest extends AnyFunSuite {
     assert(databaseUnderTest.getModes.size === 1)
     databaseUnderTest.deleteMode(0)
     assert(databaseUnderTest.getModes.size === 0)
+  }
+
+  test("testAddMode") {
+    val databaseUnderTest: ModeDatabase = new ModeDatabase()
+    assert(databaseUnderTest.getModes.size === 0)
+    databaseUnderTest.addMode()
+    assert(databaseUnderTest.getModes.size === 1)
+    val addedMode: Mode = databaseUnderTest.getModes.head
+    assert(addedMode.id === 0)
+    assert(addedMode.name.isEmpty)
+    assert(addedMode.description.isEmpty)
+    assert(addedMode.deletable === true)
+  }
+
+  test("testGetModes") {
+    val databaseUnderTest: ModeDatabase = new ModeDatabase()
+    assert(databaseUnderTest.getModes.size === 0)
+    databaseUnderTest.addMode()
+    assert(databaseUnderTest.getModes.size === 1)
+    databaseUnderTest.addMode()
+    assert(databaseUnderTest.getModes.size === 2)
+    databaseUnderTest.addMode()
+    assert(databaseUnderTest.getModes.size === 3)
+  }
+
+  test("testUpdateMode") {
+    val databaseUnderTest: ModeDatabase = new ModeDatabase()
+    assert(databaseUnderTest.getModes.size === 0)
+    databaseUnderTest.addMode()
+    assert(databaseUnderTest.getModes.size === 1)
+    val updatedMode: Mode = Mode(id = 1, "test_name", "test_description", List(), deletable = false)
+    databaseUnderTest.updateMode(0, updatedMode)
+    val checkedMode: Mode = databaseUnderTest.getModes.head
+    assert(checkedMode.id === 0)
+    assert(checkedMode.name === "test_name")
+    assert(checkedMode.description === "test_description")
+    assert(checkedMode.deletable === false)
   }
 }

33a16446ef3d7449cc21fb805219a9ada9994041
Piotr Ponikowski <piopon.github@gmail.com>
2023-05-01 12:19:27 +0200

    new negative unit tests in quiz database

--- backend/service-quiz/src/test/scala/pl/smtc/smartwords/database/QuizDatabaseTest.scala
+++ backend/service-quiz/src/test/scala/pl/smtc/smartwords/database/QuizDatabaseTest.scala
@@ -13,11 +13,27 @@ class QuizDatabaseTest extends AnyFunSuite {
     assert(result.toString.nonEmpty)
   }
 
+  test("testRandomUUID") {
+    val databaseUnderTest: QuizDatabase = new QuizDatabase()
+    val result1: UUID = databaseUnderTest.addQuiz(Quiz(List(), 0))
+    val result2: UUID = databaseUnderTest.addQuiz(Quiz(List(), 0))
+    assert(result1.toString !== result2.toString)
+  }
+
   test("testRemoveQuiz") {
     val databaseUnderTest: QuizDatabase = new QuizDatabase()
     val uuid: UUID = databaseUnderTest.addQuiz(Quiz(List(), 0))
     val result: Option[Quiz] = databaseUnderTest.removeQuiz(uuid)
     assert(result.nonEmpty)
+  }
+
+  test("testRemoveEmpty") {
+    val databaseUnderTest: QuizDatabase = new QuizDatabase()
+    val uuid: UUID = databaseUnderTest.addQuiz(Quiz(List(), 0))
+    val randomUuid: UUID = UUID.randomUUID()
+    assert(uuid.toString !== randomUuid.toString)
+    val result: Option[Quiz] = databaseUnderTest.removeQuiz(randomUuid)
+    assert(result.isEmpty)
   }
 
   test("testGetQuiz") {
@@ -27,5 +43,14 @@ class QuizDatabaseTest extends AnyFunSuite {
     assert(result.nonEmpty)
     assert(result.get.rounds.isEmpty)
     assert(result.get.score === 0)
+  }
+
+  test("testGetEmpty") {
+    val databaseUnderTest: QuizDatabase = new QuizDatabase()
+    val uuid: UUID = databaseUnderTest.addQuiz(Quiz(List(), 0))
+    val randomUuid: UUID = UUID.randomUUID()
+    assert(uuid.toString !== randomUuid.toString)
+    val result: Option[Quiz] = databaseUnderTest.getQuiz(randomUuid)
+    assert(result.isEmpty)
   }
 }

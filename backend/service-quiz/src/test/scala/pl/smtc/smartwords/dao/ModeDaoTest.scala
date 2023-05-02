984ac6d9f28d7d272fb5a5355c8a739980b46587
Piotr Ponikowski <piopon.github@gmail.com>
2023-05-02 18:47:30 +0200

    added initial getModeDecoder unit test for checking error

--- backend/service-quiz/src/test/scala/pl/smtc/smartwords/dao/ModeDaoTest.scala
+++ backend/service-quiz/src/test/scala/pl/smtc/smartwords/dao/ModeDaoTest.scala
@@ -6,17 +6,24 @@ import pl.smtc.smartwords.model.Mode
 
 class ModeDaoTest extends AnyFunSuite {
 
-  private def createTestMode(id: Int, name: String, description: String): Json = {
+  test("testGetModeDecoder") {
+    val decoderUnderTest: Decoder[Mode] = ModeDao.getModeDecoder
+    val sourceJson = createModeJson(id = 0, name = "test-mode", description = "test-mode-description")
+    val decodedValue = decoderUnderTest.decodeJson(sourceJson)
+    assert(decodedValue.left.toOption === None)
+  }
+
+  private def createModeJson(id: Int, name: String, description: String): Json = {
     Json.obj(
       ("id", Json.fromInt(id)),
       ("name",  Json.fromString(name)),
       ("description",  Json.fromString(description)),
       ("deletable",  Json.fromBoolean(true)),
-      ("settings", Json.arr(createTestSetting()))
+      ("settings", Json.arr(createSettingJson()))
     )
   }
 
-  private def createTestSetting(): Json = {
+  private def createSettingJson(): Json = {
     Json.obj(
       ("type", Json.fromString("test-setting")),
       ("label", Json.fromString("test-setting-label")),

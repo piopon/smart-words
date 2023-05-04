22d7810cc3a766a02550cbd142b56e8ab12f0ef2
Piotr Ponikowski <piopon.github@gmail.com>
2023-05-04 19:15:55 +0200

    new unit tests for quiz/round DAO class

---
+++ backend/service-quiz/src/test/scala/pl/smtc/smartwords/dao/QuizDaoTest.scala
@@ -0,0 +1,34 @@
+package pl.smtc.smartwords.dao
+
+import io.circe.{Encoder, Json}
+import org.scalatest.funsuite.AnyFunSuite
+import pl.smtc.smartwords.model.{Round, Word}
+
+class QuizDaoTest extends AnyFunSuite {
+
+  private val testWordName: String = "new-test-word"
+  private val testRoundOptions: Iterable[String] = List("option-1", "option-II", "option-C", "option-d")
+
+  test("testGetRoundEncoder") {
+    val encoderUnderTest: Encoder[Round] = QuizDao.getRoundEncoder
+    val sourceWord: Word = Word(testWordName, "test-cat", List("definition", "alt-definition"))
+    val sourceRound: Round = Round(sourceWord , testRoundOptions.toList, Some(false), Some(3))
+    val encodedValue: Json = encoderUnderTest.apply(sourceRound)
+    val expectedValue: Json = createRoundJson(testWordName)
+    assert(encodedValue === expectedValue)
+  }
+
+  /**
+   * Method used to create quiz round JSON object with hardcoded options, answer, correct flag, and customizable word
+   * @param word word name which is used in the quiz round
+   * @return JSON object representing quiz round
+   */
+  private def createRoundJson(word: String): Json = {
+    Json.obj(
+      ("word", Json.fromString(word)),
+      ("options", Json.fromValues(testRoundOptions.map(option => Json.fromString(option)))),
+      ("correct", Json.fromBoolean(false)),
+      ("answer", Json.fromInt(3))
+    )
+  }
+}

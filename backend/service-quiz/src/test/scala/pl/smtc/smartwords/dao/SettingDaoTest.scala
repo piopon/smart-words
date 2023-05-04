ce879117198ba39eb826a1802a02b428f64a5494
Piotr Ponikowski <piopon.github@gmail.com>
2023-05-04 18:38:52 +0200

    added setting decoder and encoder unit tests

--- backend/service-quiz/src/test/scala/pl/smtc/smartwords/dao/SettingDaoTest.scala
+++ backend/service-quiz/src/test/scala/pl/smtc/smartwords/dao/SettingDaoTest.scala
@@ -6,6 +6,26 @@ import pl.smtc.smartwords.model.{Kind, Setting}
 
 class SettingDaoTest extends AnyFunSuite {
 
+  test("testGetSettingDecoder") {
+    val decoderUnderTest: Decoder[Setting] = SettingDao.getSettingDecoder
+    val sourceJson: Json = createSettingJson()
+    val decodedValue: Decoder.Result[Setting] = decoderUnderTest.decodeJson(sourceJson)
+    assert(decodedValue.left.toOption === None)
+    assert(decodedValue.toOption !== None)
+    val decodedSetting: Setting = decodedValue.toOption.get
+    assert(decodedSetting.kind === Kind.languages)
+    assert(decodedSetting.label === "test-setting-label")
+    assert(decodedSetting.details === "test-setting-details")
+  }
+
+  test("testGetSettingEncoder") {
+    val encoderUnderTest: Encoder[Setting] = SettingDao.getSettingEncoder
+    val sourceSetting: Setting = Setting(Kind.languages, "test-setting-label", "test-setting-details")
+    val encodedValue: Json = encoderUnderTest.apply(sourceSetting)
+    val expectedValue: Json = createSettingJson()
+    assert(encodedValue === expectedValue)
+  }
+
   /**
    * Method used to create mode settings JSON object with hardcoded type, label, and details
    * @return JSON object representing mode setting

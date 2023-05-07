5538d0a47ec80b8ff12429ec1892d2eee1a340d3
Piotr Ponikowski <piopon.github@gmail.com>
2023-05-07 16:44:11 +0200

    updated test implementation of word service to fix quiz service test choke

--- backend/service-quiz/src/test/scala/pl/smtc/smartwords/client/WordServiceTest.scala
+++ backend/service-quiz/src/test/scala/pl/smtc/smartwords/client/WordServiceTest.scala
@@ -2,6 +2,8 @@ package pl.smtc.smartwords.client
 
 import pl.smtc.smartwords.model._
 
+import scala.util.Random
+
 /**
  * <b>IMPORTANT:</b> This class is a test implementation of a word client service, not a class with unit tests
  */
@@ -10,14 +12,23 @@ class WordServiceTest extends IWordService {
   override def isAlive: Boolean = true
 
   override def getRandomWord(mode: Int, language: String): Word = {
-    Word("word-" + language + "-" + mode.toString, "verb", List("definition-main", "alternate-definition"))
+    val randomIdString: String = new Random().nextInt.toString
+    val randomWordName: String = "word-" + language + "-" + mode.toString + randomIdString
+    Word(randomWordName, "verb", List("definition-main-" + randomIdString, "alternate-definition-" + randomIdString))
   }
 
   override def getWordsByCategory(mode: Int, language: String, category: String): List[Word] = {
     List(
-      Word("word-" + language + "-" + mode.toString + "-1", category, List("def-1", "alt-11")),
-      Word("word-" + language + "-" + mode.toString + "-2", category, List("def-2", "alt-22")),
-      Word("word-" + language + "-" + mode.toString + "-3", category, List("def-3", "alt-33"))
+      Word("word-" + language + "-" + mode.toString + "-00", category, List("def-0", "alt-00")),
+      Word("word-" + language + "-" + mode.toString + "-01", category, List("def-1", "alt-11")),
+      Word("word-" + language + "-" + mode.toString + "-02", category, List("def-2", "alt-22")),
+      Word("word-" + language + "-" + mode.toString + "-03", category, List("def-3", "alt-33")),
+      Word("word-" + language + "-" + mode.toString + "-04", category, List("def-4", "alt-44")),
+      Word("word-" + language + "-" + mode.toString + "-05", category, List("def-5", "alt-55")),
+      Word("word-" + language + "-" + mode.toString + "-06", category, List("def-6", "alt-66")),
+      Word("word-" + language + "-" + mode.toString + "-07", category, List("def-7", "alt-77")),
+      Word("word-" + language + "-" + mode.toString + "-08", category, List("def-8", "alt-88")),
+      Word("word-" + language + "-" + mode.toString + "-09", category, List("def-9", "alt-99"))
     )
   }
 }

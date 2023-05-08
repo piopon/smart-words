5440e8e641e51b88038626598e383b27a2191065
Piotr Ponikowski <piopon.github@gmail.com>
2023-05-08 21:48:46 +0200

    added UUID regex to correctly check start quiz test

--- backend/service-quiz/src/test/scala/pl/smtc/smartwords/service/QuizServiceTest.scala
+++ backend/service-quiz/src/test/scala/pl/smtc/smartwords/service/QuizServiceTest.scala
@@ -7,11 +7,13 @@ import pl.smtc.smartwords.database.QuizDatabase
 
 class QuizServiceTest extends AnyFunSuite {
 
+  private val uuidRegex: String ="^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$"
+
   test("testStartQuiz") {
     val quizDatabase: QuizDatabase = new QuizDatabase
     val wordService: WordServiceTest = new WordServiceTest
     val serviceUnderTest: QuizService = new QuizService(quizDatabase, wordService)
     val res: String = serviceUnderTest.startQuiz(Some(5), Some(1), Some("pl")).flatMap(_.as[String]).unsafeRunSync()
-    assert(res.nonEmpty)
+    assert(res.matches(uuidRegex))
   }
 }

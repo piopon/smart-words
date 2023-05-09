af3b6e50491ad4a0edc90294498926310bbe7cdb
Piotr Ponikowski <piopon.github@gmail.com>
2023-05-09 22:29:13 +0200

    added test method to verify the quiz service getQuizQuestionNo() method

--- backend/service-quiz/src/test/scala/pl/smtc/smartwords/service/QuizServiceTest.scala
+++ backend/service-quiz/src/test/scala/pl/smtc/smartwords/service/QuizServiceTest.scala
@@ -1,9 +1,14 @@
 package pl.smtc.smartwords.service
 
 import cats.effect.unsafe.implicits.global
+import io.circe.Json
+import org.http4s.circe.jsonDecoder
 import org.scalatest.funsuite.AnyFunSuite
 import pl.smtc.smartwords.client.WordServiceTest
 import pl.smtc.smartwords.database.QuizDatabase
+import pl.smtc.smartwords.model.Round
+
+import java.util.UUID
 
 class QuizServiceTest extends AnyFunSuite {
 
@@ -15,5 +20,22 @@ class QuizServiceTest extends AnyFunSuite {
     val serviceUnderTest: QuizService = new QuizService(quizDatabase, wordService)
     val res: String = serviceUnderTest.startQuiz(Some(5), Some(1), Some("pl")).flatMap(_.as[String]).unsafeRunSync()
     assert(res.matches(uuidRegex))
+  }
+
+  test("testGetQuizQuestionNo") {
+    val quizDatabase: QuizDatabase = new QuizDatabase
+    val wordService: WordServiceTest = new WordServiceTest
+    val serviceUnderTest: QuizService = new QuizService(quizDatabase, wordService)
+    val start: String = serviceUnderTest.startQuiz(Some(5), Some(72), Some("es")).flatMap(_.as[String]).unsafeRunSync()
+    val uuid: UUID = UUID.fromString(start)
+    val res: Json = serviceUnderTest.getQuizQuestionNo(uuid, "1").flatMap(_.as[Json]).unsafeRunSync()
+    assert(res.hcursor.downField("word").as[String] match {
+      case Right(s) => s.startsWith("word-es-72")
+      case Left(_) => false
+    })
+    assert(res.hcursor.downField("options").as[Array[String]] match {
+      case Right(s) => s.length == 4 && (s.head.startsWith("def") || s.head.startsWith("alt"))
+      case Left(_) => false
+    })
   }
 }

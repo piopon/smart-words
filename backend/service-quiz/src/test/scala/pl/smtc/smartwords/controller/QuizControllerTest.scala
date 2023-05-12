48023f6b440f9653f1dec8c5b3e1480503dd066d
Piotr Ponikowski <piopon.github@gmail.com>
2023-05-13 00:42:15 +0200

    fixed build errors in quiz controller test class (added missing ctor params)

--- backend/service-quiz/src/test/scala/pl/smtc/smartwords/controller/QuizControllerTest.scala
+++ backend/service-quiz/src/test/scala/pl/smtc/smartwords/controller/QuizControllerTest.scala
@@ -4,11 +4,15 @@ import cats.effect.IO
 import cats.effect.unsafe.implicits.global
 import org.http4s.{Method, Request, Response, Uri}
 import org.scalatest.funsuite.AnyFunSuite
+import pl.smtc.smartwords.client.WordServiceTest
+import pl.smtc.smartwords.database.QuizDatabase
 
 class QuizControllerTest extends AnyFunSuite {
 
   test("testGetRoutesReturnsNoneForNonExistingEndpoint") {
-    val controllerUnderTest: QuizController = new QuizController()
+    val quizDatabase: QuizDatabase = new QuizDatabase()
+    val wordService: WordServiceTest = new WordServiceTest
+    val controllerUnderTest: QuizController = new QuizController(quizDatabase, wordService)
     val request: Request[IO] = Request(Method.POST, Uri.unsafeFromString("/non-existing"))
     val response: Option[Response[IO]] = controllerUnderTest.getRoutes.run(request).value.unsafeRunSync()
     assert(response.isEmpty)

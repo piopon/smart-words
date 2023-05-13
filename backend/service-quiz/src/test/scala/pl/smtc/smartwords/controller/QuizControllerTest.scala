4a27179f82bc7c8bcb54dd5a349588e73832cd18
Piotr Ponikowski <piopon.github@gmail.com>
2023-05-14 00:13:55 +0200

    added routes unit check verifying quiz start logic with no optionals

--- backend/service-quiz/src/test/scala/pl/smtc/smartwords/controller/QuizControllerTest.scala
+++ backend/service-quiz/src/test/scala/pl/smtc/smartwords/controller/QuizControllerTest.scala
@@ -9,6 +9,8 @@ import pl.smtc.smartwords.database._
 
 class QuizControllerTest extends AnyFunSuite {
 
+  private val uuidRegex: String = "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$"
+
   test("testGetRoutesReturnsNoneForNonExistingEndpoint") {
     val quizDatabase: QuizDatabase = new QuizDatabase()
     val wordService: WordServiceTest = new WordServiceTest
@@ -16,5 +18,16 @@ class QuizControllerTest extends AnyFunSuite {
     val request: Request[IO] = Request(Method.POST, Uri.unsafeFromString("/non-existing"))
     val response: Option[Response[IO]] = controllerUnderTest.getRoutes.run(request).value.unsafeRunSync()
     assert(response.isEmpty)
+  }
+
+  test("testGetRoutesReturnsCorrectResponseOnEndpointStart") {
+    val quizDatabase: QuizDatabase = new QuizDatabase()
+    val wordService: WordServiceTest = new WordServiceTest
+    val controllerUnderTest: QuizController = new QuizController(quizDatabase, wordService)
+    val request: Request[IO] = Request(Method.POST, Uri.unsafeFromString("/start"))
+    val response: Option[Response[IO]] = controllerUnderTest.getRoutes.run(request).value.unsafeRunSync()
+    assert(response.nonEmpty)
+    assert(response.get.status === Status.Ok)
+    assert(response.get.as[String].unsafeRunSync.matches(uuidRegex))
   }
 }

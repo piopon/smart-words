2a5ef3c7a0dc288973a9797f9d49a1e436afe34e
Piotr Ponikowski <piopon.github@gmail.com>
2023-05-15 23:27:28 +0200

    verifying result when sending invalid (outside range) value when getting question

--- backend/service-quiz/src/test/scala/pl/smtc/smartwords/controller/QuizControllerTest.scala
+++ backend/service-quiz/src/test/scala/pl/smtc/smartwords/controller/QuizControllerTest.scala
@@ -145,4 +145,21 @@ class QuizControllerTest extends AnyFunSuite {
     val actualBody: String = response.get.as[String].unsafeRunSync
     assert(actualBody === "Question number must be of integer type.")
   }
+
+  test("testGetRoutesReturnsBadRequestWhenGettingQuestionHasInvalidValue") {
+    val quizDatabase: QuizDatabase = new QuizDatabase()
+    val wordService: WordServiceTest = new WordServiceTest
+    val controllerUnderTest: QuizController = new QuizController(quizDatabase, wordService)
+    val quizUuid: String = controllerUnderTest.getRoutes.run(Request(Method.POST, Uri.unsafeFromString(s"/start")))
+                                                        .value.unsafeRunSync()
+                                                        .get.as[String].unsafeRunSync
+    val endpoint: String = s"/${UUID.fromString(quizUuid)}/question/10"
+    val request: Request[IO] = Request(Method.GET, Uri.unsafeFromString(endpoint))
+    val response: Option[Response[IO]] = controllerUnderTest.getRoutes.run(request).value.unsafeRunSync()
+    assert(response.nonEmpty)
+    val actualStatus: Status = response.get.status
+    assert(actualStatus === Status.BadRequest)
+    val actualBody: String = response.get.as[String].unsafeRunSync
+    assert(actualBody === "Question number must have value between 0-9")
+  }
 }

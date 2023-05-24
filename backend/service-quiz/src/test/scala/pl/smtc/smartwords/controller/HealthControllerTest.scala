11f760ab58eb7079fe78881fa6fe1f4de77b4c03
Piotr Ponikowski <piopon.github@gmail.com>
2023-05-24 19:58:58 +0200

    added final service quiz unit test for health controller

--- backend/service-quiz/src/test/scala/pl/smtc/smartwords/controller/HealthControllerTest.scala
+++ backend/service-quiz/src/test/scala/pl/smtc/smartwords/controller/HealthControllerTest.scala
@@ -14,4 +14,14 @@ class HealthControllerTest extends AnyFunSuite {
     val response: Option[Response[IO]] = controllerUnderTest.getRoutes.run(request).value.unsafeRunSync()
     assert(response.isEmpty)
   }
+
+  test("testGetRoutesReturnsOkStatusForCheckingServiceHealth") {
+    val controllerUnderTest: HealthController = new HealthController()
+    val endpoint: String = s"/"
+    val request: Request[IO] = Request(Method.GET, Uri.unsafeFromString(endpoint))
+    val response: Option[Response[IO]] = controllerUnderTest.getRoutes.run(request).value.unsafeRunSync()
+    val actualStatus: Status = response.get.status
+    assert(actualStatus === Status.Ok)
+    assert(response.get.as[String].unsafeRunSync === "Service: QUIZ - status: OK")
+  }
 }

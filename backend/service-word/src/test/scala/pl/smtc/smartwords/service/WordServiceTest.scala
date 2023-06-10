c6dea5598bb88476df9072a312e987944e0ad425
Piotr Ponikowski <piopon.github@gmail.com>
2023-06-10 13:30:28 +0200

    new unit tests for deleting incorrect variants of existing word

--- backend/service-word/src/test/scala/pl/smtc/smartwords/service/WordServiceTest.scala
+++ backend/service-word/src/test/scala/pl/smtc/smartwords/service/WordServiceTest.scala
@@ -180,6 +180,27 @@ class WordServiceTest extends AnyFunSuite {
     assert(res === "word 'word-10-de' not found in DB")
   }
 
+  test("testDeleteWordReturnsErrorWhenDeletingWordWithCorrectModeLangButBadName") {
+    val serviceUnderTest: WordService = new WordService(createTestDatabase())
+    val res: String = serviceUnderTest.deleteWord(Some(997), "de", "word-10-de")
+                                      .flatMap(_.as[String]).unsafeRunSync()
+    assert(res === "word 'word-10-de' not found in DB")
+  }
+
+  test("testDeleteWordReturnsErrorWhenDeletingWordWithCorrectModeNameButBadLang") {
+    val serviceUnderTest: WordService = new WordService(createTestDatabase())
+    val res: String = serviceUnderTest.deleteWord(Some(997), "fr", "word-1-de")
+                                      .flatMap(_.as[String]).unsafeRunSync()
+    assert(res === "word 'word-1-de' not found in DB")
+  }
+
+  test("testDeleteWordReturnsErrorWhenDeletingWordWithCorrectNameLangButBadMode") {
+    val serviceUnderTest: WordService = new WordService(createTestDatabase())
+    val res: String = serviceUnderTest.deleteWord(Some(123), "de", "word-1-de")
+                                      .flatMap(_.as[String]).unsafeRunSync()
+    assert(res === "word 'word-1-de' not found in DB")
+  }
+
   private def createTestDatabase(): WordDatabase = {
     val database: WordDatabase = new WordDatabase()
     val dictionaryPl: Dictionary = Dictionary(serviceTestFile, "quiz", Some(999), "pl")

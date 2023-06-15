d21f99d55a8fa3b770734c7c6f582a344e09b42a
Piotr Ponikowski <piopon.github@gmail.com>
2023-06-16 00:14:23 +0200

    checking bad request response when asking for non existing mode

--- backend/service-word/src/test/scala/pl/smtc/smartwords/controller/WordControllerTest.scala
+++ backend/service-word/src/test/scala/pl/smtc/smartwords/controller/WordControllerTest.scala
@@ -22,6 +22,15 @@ class WordControllerTest extends AnyFunSuite {
     assert(response.isEmpty)
   }
 
+  test("testGetRoutesReturnsBadRequestWhenGettingWordsWithNonExistingMode") {
+    val controllerUnderTest: WordController = new WordController(createTestDatabase())
+    val endpoint: String = s"/111/pl"
+    val request: Request[IO] = Request(Method.GET, Uri.unsafeFromString(endpoint))
+    val response: Option[Response[IO]] = controllerUnderTest.getRoutes.run(request).value.unsafeRunSync()
+    val actualStatus: Status = response.get.status
+    assert(actualStatus === Status.BadRequest)
+  }
+
   test("testGetRoutesReturnsCorrectResponseWhenGettingWordsWithExistingModeAndLanguage") {
     val controllerUnderTest: WordController = new WordController(createTestDatabase())
     val endpoint: String = s"/999/pl"

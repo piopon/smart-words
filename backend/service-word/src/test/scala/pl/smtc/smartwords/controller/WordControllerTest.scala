e33b9f09c0717868d579c48dcfacf8887c00b507
Piotr Ponikowski <piopon.github@gmail.com>
2023-06-20 00:12:44 +0200

    verifying PUT request when updating correct existing word

--- backend/service-word/src/test/scala/pl/smtc/smartwords/controller/WordControllerTest.scala
+++ backend/service-word/src/test/scala/pl/smtc/smartwords/controller/WordControllerTest.scala
@@ -272,6 +272,18 @@ class WordControllerTest extends AnyFunSuite {
     assert(response.get.as[String].unsafeRunSync === "Invalid 'mode' parameter: value 'mode' cannot be parsed.")
   }
 
+  test("testGetRoutesReturnsCorrectResponseWhenUpdatingExistingWord") {
+    val controllerUnderTest: WordController = new WordController(createTestDatabase())
+    val endpoint: String = s"/997/de/word-1-de"
+    val requestBody: Json = json"""{ "name" : "word-10-de", "category" : "person", "description" : [""] }"""
+    val request: Request[IO] = Request(Method.PUT, Uri.unsafeFromString(endpoint)).withEntity(requestBody)
+    val response: Option[Response[IO]] = controllerUnderTest.getRoutes.run(request).value.unsafeRunSync()
+    assert(response.nonEmpty)
+    val actualStatus: Status = response.get.status
+    assert(actualStatus === Status.Ok)
+    assert(response.get.as[String].unsafeRunSync === "updated word 'word-1-de'")
+  }
+
   private def createTestDatabase(): WordDatabase = {
     val database: WordDatabase = new WordDatabase()
     val dictionaryPl: Dictionary = Dictionary(serviceTestFile, "quiz", Some(999), "pl")

671f6fe7e4dae71e0bcee2bc14a53d50878cdd07
Piotr Ponikowski <piopon.github@gmail.com>
2023-06-21 18:19:38 +0200

    testing bad request response when updating word using bad lang and mode

--- backend/service-word/src/test/scala/pl/smtc/smartwords/controller/WordControllerTest.scala
+++ backend/service-word/src/test/scala/pl/smtc/smartwords/controller/WordControllerTest.scala
@@ -320,6 +320,30 @@ class WordControllerTest extends AnyFunSuite {
     assert(response.get.as[String].unsafeRunSync === "word 'word-1-de' not found in DB")
   }
 
+  test("testGetRoutesReturnsBadRequestWhenUpdatingExistingWordWithBadLanguage") {
+    val controllerUnderTest: WordController = new WordController(createTestDatabase())
+    val endpoint: String = s"/997/es/word-1-de"
+    val requestBody: Json = json"""{ "name" : "word-10-de", "category" : "person", "description" : [""] }"""
+    val request: Request[IO] = Request(Method.PUT, Uri.unsafeFromString(endpoint)).withEntity(requestBody)
+    val response: Option[Response[IO]] = controllerUnderTest.getRoutes.run(request).value.unsafeRunSync()
+    assert(response.nonEmpty)
+    val actualStatus: Status = response.get.status
+    assert(actualStatus === Status.BadRequest)
+    assert(response.get.as[String].unsafeRunSync === "Invalid 'language' parameter: value 'es' is not supported.")
+  }
+
+  test("testGetRoutesReturnsBadRequestWhenUpdatingExistingWordWithBadMode") {
+    val controllerUnderTest: WordController = new WordController(createTestDatabase())
+    val endpoint: String = s"/900/de/word-1-de"
+    val requestBody: Json = json"""{ "name" : "word-10-de", "category" : "person", "description" : [""] }"""
+    val request: Request[IO] = Request(Method.PUT, Uri.unsafeFromString(endpoint)).withEntity(requestBody)
+    val response: Option[Response[IO]] = controllerUnderTest.getRoutes.run(request).value.unsafeRunSync()
+    assert(response.nonEmpty)
+    val actualStatus: Status = response.get.status
+    assert(actualStatus === Status.BadRequest)
+    assert(response.get.as[String].unsafeRunSync === "Invalid 'mode' parameter: value '900' is not supported.")
+  }
+
   private def createTestDatabase(): WordDatabase = {
     val database: WordDatabase = new WordDatabase()
     val dictionaryPl: Dictionary = Dictionary(serviceTestFile, "quiz", Some(999), "pl")
